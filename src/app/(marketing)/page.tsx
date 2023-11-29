f105c10d114298f2da0fe5fe479bd4b43090cac3
NeilRow56 <neilrowland56@gmail.com>
2023-11-29 07:08:30 +0000

    Added local and google fonts to marketing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,14 +1,31 @@
-import { Medal } from 'lucide-react'
-
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import localFont from 'next/font/local'
+import { Medal } from 'lucide-react'
+import { Poppins } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 
 type Props = {}
+
+const headingFont = localFont({
+  src: '../../../public/fonts/font.woff2',
+})
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 const MarketingPage = (props: Props) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
+      <div
+        className={cn(
+          'flex flex-col items-center justify-center',
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center rounded-full border bg-amber-100 p-4 uppercase text-amber-700 shadow-sm">
           <Medal className="mr-2 h-6 w-6" />
           No 1 Task Management
@@ -20,7 +37,12 @@ const MarketingPage = (props: Props) => {
           work forward.
         </div>
       </div>
-      <div className="mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl">
+      <div
+        className={cn(
+          'mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects and reach new productivity peeks. From high
         rises to the home office, the way your team works is unique - accomplish
         it all with Taskify.
