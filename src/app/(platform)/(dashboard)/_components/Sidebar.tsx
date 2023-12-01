c9cd7dd853b38d302a8d3615bef1c250ce5071f4
NeilRow56 <neilrowland56@gmail.com>
2023-12-01 11:34:15 +0000

    Added sidebar and mobileSidebar

--- src/app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -2,12 +2,66 @@
 
 import Link from 'next/link'
 import { Plus } from 'lucide-react'
+import { useLocalStorage } from 'usehooks-ts'
+import { useOrganization, useOrganizationList } from '@clerk/nextjs'
 
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { Accordion } from '@/components/ui/accordion'
+import { NavItem, Organization } from './NavItem'
 
-type Props = {}
+interface SidebarProps {
+  storageKey?: string
+}
 
-export const Sidebar = (props: Props) => {
+export const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  )
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization()
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  })
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key)
+      }
+
+      return acc
+    },
+    []
+  )
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }))
+  }
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <div className="mb-2 flex items-center justify-between">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="mb-1 flex items-center text-xs font-medium">
@@ -24,6 +78,21 @@ export const Sidebar = (props: Props) => {
           </Link>
         </Button>
       </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
     </>
   )
 }
